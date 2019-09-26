9d34d7a054a518d521aa7d02436f483f13560c67
Erik Nelson <erik@nsk.io>
2019-09-26 10:27:12 -0400

    Update example image tags to latest

--- deploy/test-helpers/my_var.ex
+++ deploy/test-helpers/my_var.ex
@@ -4,9 +4,9 @@ DOWNSTREAM_REPONAME=rhcam
 # NOTE: TARGET_REPONAME *MUST* BE AN EXISTING NAMESPACE IN THE TARGET CLUSTER
 TARGET_REPONAME=openshift
 IMAGES=(
-  "openshift-migration-controller:v1.0-0.6"
+  "openshift-migration-controller:v1.0-0.7"
   "openshift-migration-operator:v1.0-0.6"
-  "openshift-migration-plugin:v1.0-0.1"
+  "openshift-migration-plugin:v1.0-0.2"
   "openshift-migration-ui:v1.0-0.13"
   "openshift-migration-velero:v1.0-0.9"
   "openshift-migration-velero-restic-restore-helper:v1.0-0.1"
